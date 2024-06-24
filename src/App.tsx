5c5a0fce69aa962489b0564de62a94d94b9ba267
takumu1011 <sumikawa.web@gmail.com>
2024-06-24 20:55:06 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,61 @@
+import { useMemo, useState } from "react";
+import './App.css';
+
+export default function App() {
+  const [show, setShow] = useState(false);
+  const [count, setCount] = useState(0);
+
+  const divRef = useMemo(() => {
+    let cleanup: (() => void) | undefined;
+    return (div: HTMLDivElement | null) => {
+      if (div === null) {
+        cleanup?.();
+        return;
+      }
+      const controller = new AbortController();
+      document.addEventListener('click', (event) => {
+        if (div.contains(event.target as Node)) return;
+        setCount((count) => count + 1);
+      }, {
+        signal: controller.signal
+      });
+      cleanup = () => {
+        controller.abort();
+      }
+    }
+  }, []);
+
+  // const divRef = useRef<HTMLDivElement | null>(null);
+  // useEffect(() => {
+  //   const div = divRef.current;
+  //   if (div === null) return;
+  //   const controller = new AbortController();
+  //   document.addEventListener('click', (event) => {
+  //     if (div.contains(event.target as Node)) return;
+  //     setCount((count) => count + 1);
+  //   }, {
+  //     signal: controller.signal
+  //   });
+  //   return () => {
+  //     controller.abort();
+  //   }
+  // }, []);
+
+  return (
+    <div>
+      <label>
+        <input type="checkbox" checked={show} onChange={(e) => {
+          setShow(e.currentTarget.checked);
+        }} />
+        Show
+      </label>
+      <p>count = {count}</p>
+      {show && (
+        <div className="square" ref={divRef}>
+          div!
+        </div>
+      )}
+    </div>
+  )
+
+}
